3022621be6c7f412b49b5b2e9707543e169cd8c8
EJramirez <eramirezp26@miumg.edu.gt>
2023-02-28 10:23:22 -0600

    Javs3

---
+++ APRENDER JAVA/Tipos de Variables.js
@@ -0,0 +1,47 @@
+// 0. Declarar una variable var cajon;
+
+// 1. String Variable tipo texto
+
+var bootcamp = "Eduardo Josue";
+
+// 2. int variable tipo numero
+
+var numeroEntero = 10;
+
+var numeroDecimal = 10.5
+
+var numeroNegativo = -10;
+
+// 3. Boolean variable tipo verdaro o falso 
+
+var Entretenido = true;
+
+var Aburrido = false;
+
+// 4. undefined variable sin valor indefinido
+
+var indefinido;
+
+console.log(indefinido);
+
+// 5. null variable sin valor nulo
+
+var nulo = null;
+
+// git clone es para clonar un repositorio
+// gir add . es para agregar los cambios
+// git commit -m "mensaje" es para guardar los cambios
+// git push es para subir los cambios
+// git pull es para bajar los cambios
+// git status es para ver el estado de los cambios
+// git log es para ver el historial de los cambios
+// git checkout es para cambiar de rama
+// git checkout -b es para crear una rama
+// git branch es para ver las ramas
+// git merge es para unir ramas
+// git branch -d es para borrar una rama
+// git remote add origin es para agregar un repositorio remoto
+// git remote -v es para ver los repositorios remotos
+// git push -u origin master es para subir los cambios al repositorio remoto
+// git push origin master es para subir los cambios al repositorio remoto
+// git pull origin master es para bajar los cambios del repositorio remoto
